444d8fe1a26282985bf1fc87a10442bf8ff9d7ee
Jamal Shareef <theepie@hotmail.com>
2019-04-08 23:53:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,15 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.collect do |season_number, info|
+    if season_number.split[1].to_i == season
+      info.collect do |key, value|
+        
+            return data[season_number]["name"]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
