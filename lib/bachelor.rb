4de723ebb08d5d5860ff205aad7713a1f6f32b6c
Jamal Shareef <theepie@hotmail.com>
2019-04-09 00:06:13 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,9 +3,8 @@ def get_first_name_of_season_winner(data, season)
   data.collect do |season_number, info|
     if season_number.split[1].to_i == season
       info.collect do |hash|
-        if hash["status"] = "Winner"
-          return data[season_number]["name"]
-          end
+        if hash["status"] == "Winner"
+          return data[season_number][hash]["name"].split[1]
         end
       end
     end
@@ -14,16 +13,48 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  array = Array.new
+  data.collect do |season_number, info|
+    info.collect do |hash|
+      if hash["occupation"] == occupation
+        array.push(hash["name"])
+      end
+    end
+  end 
+  array
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter = 0
+  data.collect do |season_number, info|
+    info.collect do |hash|
+      if hash["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.collect do |season_number, info|
+    info.collect do |hash|
+      if hash["hometown"] == hometown
+        return hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  contestants = 0
+  total = 0
+  
+  data.collect do |season_number, info|
+    if season_number.split[1].to_i == season
+      info.collect do |hash|
+        total += hash["age"].to_i
+        contestants += 1
 end
